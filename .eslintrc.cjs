f5de34b1861f66062b4ab93249975e99a78f3ccd
fowlerro <fwlerro@gmail.com>
2024-02-27 00:25:19 +0100

    add linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,53 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: [
+		"plugin:react/recommended",
+		"plugin:react-hooks/recommended",
+		"airbnb",
+		"airbnb/hooks",
+		"plugin:jsx-a11y/recommended",
+		"prettier",
+	],
+	plugins: ["jsx-a11y", "prettier", "@typescript-eslint", "react-refresh"],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	settings: {
+		"import/resolver": {
+			typescript: {
+				project: ["./tsconfig.json"],
+			},
+		},
+	},
+	rules: {
+		"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+		"no-unused-vars": "warn",
+		"react/react-in-jsx-scope": "off",
+		"react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+		"react/require-default-props": ["off"],
+		"react/jsx-key": "error",
+		"react/jsx-props-no-spreading": "off",
+		"import/extensions": [
+			"error",
+			"ignorePackages",
+			{
+				js: "never",
+				jsx: "never",
+				ts: "never",
+				tsx: "never",
+			},
+		],
+		"import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+	},
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
-
 function App() {
-  return <div>Hello World</div>;
+	return <div>Hello World</div>
 }
 
-export default App;
+export default App
